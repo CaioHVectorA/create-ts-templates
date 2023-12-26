9265dea79da003d00306057144028ab611d328cd
Your Name <caihebatista@gmail.com>
2023-12-26 14:42:58 -0300

    convertion to array

--- app.ts
+++ app.ts
@@ -7,16 +7,16 @@ app.use(express.json())
 const PORT = 3000
 
 type User = { email: string, password: string, username: string }
-const users = new Map<number, User>()
+const users = [] as User[]
 
 app.get('/user', (req: Request, res: Response) => {
-    res.json(Array.from(users).map(v => v[1]))
+    res.json(users)
 })
 
 app.post('/user',(req: Request, res: Response) => {
     const data = req.body as User
     let alreadyExists = false;
-    for (let i = 0; i !== users.size ; i++) {
+    for (let i = 0; i !== users.length ; i++) {
         const user = Array.from(users.values())[i]
         console.log(user.email, data.email)
         if (!user) break
@@ -26,7 +26,7 @@ app.post('/user',(req: Request, res: Response) => {
         }
     }
     if (alreadyExists) return res.status(400).json({ status: "ERROR", cause: 'User already exists!' })
-    users.set(users.size + 1, data)
+    users.push(data)
     res.status(201).json({ status: 'OK' })
 })
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`))
